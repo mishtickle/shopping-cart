3ab7b198b27872af67fa72e23f547939492839bc
Hamish <hamishok@gmail.com>
2023-07-22 04:47:37 +1200

    updated tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import HomePage from "./components/HomePage.js";
@@ -10,7 +11,21 @@ import ShopPage from "./components/ShopPage.js";
 
 describe("Home page component", () => {
   it("renders the home page", () => {
-    const { homepage } = render(<HomePage />);
-    expect(homepage).toMatchSnapshot();
+    const { container } = render(<HomePage />, {wrapper: BrowserRouter}) ;
+    expect(container).toMatchSnapshot();
+  })
+})
+
+describe("Contact page component", () => {
+  it("renders the contact page", () => {
+    const { container } = render(<Contact />, {wrapper: BrowserRouter}) ;
+    expect(container).toMatchSnapshot();
+  })
+})
+
+describe("About page component", () => {
+  it("renders the about page", () => {
+    const { container } = render(<About />, {wrapper: BrowserRouter}) ;
+    expect(container).toMatchSnapshot();
   })
 })
